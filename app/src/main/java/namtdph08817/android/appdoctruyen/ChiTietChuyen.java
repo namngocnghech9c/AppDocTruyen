ade53cb8bc9f02a7027324dddc9c0420f4e4c8c1
pc <hoangja12@gmail.com>
2023-09-22 19:58:57 +0700

    Giao diện day22/9

---
+++ app/src/main/java/namtdph08817/android/appdoctruyen/ChiTietChuyen.java
@@ -0,0 +1,14 @@
+package namtdph08817.android.appdoctruyen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChiTietChuyen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet_chuyen);
+    }
+}
